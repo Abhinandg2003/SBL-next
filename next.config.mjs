890bc0390752d6ea5f6ca7ae48f5837c6b514230
Abhinandg2003 <abhinandg2003@gmail.com>
2026-06-25 09:42:21 +0530

    Add GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/SBL-next",
+  assetPrefix: "/SBL-next/",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
